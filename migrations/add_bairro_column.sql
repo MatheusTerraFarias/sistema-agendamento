387d5030e9a0f1ff398a4670f1c15dba40ef106e
Zeus0802 <matheusterra0802@gmail.com>
2026-08-05 11:32:32 -0300

    teste

---
+++ migrations/add_bairro_column.sql
@@ -0,0 +1,10 @@
+﻿-- Adicionar coluna bairro na tabela agendamentos
+-- Execute este SQL no editor SQL do Supabase antes de importar novamente
+
+ALTER TABLE public.agendamentos
+  ADD COLUMN IF NOT EXISTS bairro text DEFAULT NULL;
+
+-- Migrar dados existentes: copiar area para bairro onde bairro está vazio
+UPDATE public.agendamentos
+  SET bairro = area
+  WHERE bairro IS NULL AND area IS NOT NULL;
